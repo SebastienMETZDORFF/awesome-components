6206b656de4b07b007c13da370e05fdcc3dc0256
SebastienMETZDORFF <sebmetzdorff@gmail.com>
2024-04-10 14:35:03 +0200

    Récupération des candidats

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CandidatesService } from '../../services/candidates.service';
+import { Observable } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CandidateListComponent implements OnInit {
+  loading$!: Observable<boolean>;
+  candidates$!: Observable<Candidate[]>;
+
+  constructor(private candidatesService: CandidatesService) {}
+
+  ngOnInit(): void {
+    this.initObservables();
+    this.candidatesService.getCandidatesFromServer();
+  }
+
+  private initObservables(): void {
+    this.loading$ = this.candidatesService.loading$;
+    this.candidates$ = this.candidatesService.candidates$;
+  }
+}
